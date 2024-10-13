ef507ee863171fb380d202bac1f6e519bd5af9e0
Sahil-Iqubal <sahiliqubal06@gmail.com>
2024-10-13 23:25:16 +0530

    Added Javascript and HTML files for various challenges (26-28).

---
+++ 26.js
@@ -0,0 +1,46 @@
+// Implement a function that makes multiple API calls and processes the data using callbacks
+
+// function makeAPICall(url, callback) {
+//   fetch(url)
+//     .then(response => response.json())
+//     .then((data) => callback(data))
+//     .catch((err) => console.error("Error during API call:", err.message));
+// }
+
+// function handleData(data) {
+//   console.log("Processed Data", data);
+// }
+
+// function main() {
+//   const apiUrl1 = "https://jsonplaceholder.typicode.com/posts/1";
+//   const apiUrl2 = "https://jsonplaceholder.typicode.com/posts/2";
+
+//   makeAPICall(apiUrl1, handleData);
+//   makeAPICall(apiUrl2, handleData);
+// }
+
+// main();
+
+// Same Using Promises
+
+function makeAPICall(url) {
+  return fetch(url).then((response) => response.json());
+}
+
+function handleData(data) {
+  console.log("Processed Data", data);
+}
+
+function main() {
+  const apiUrl1 = "https://jsonplaceholder.typicode.com/posts/1";
+  const apiUrl2 = "https://jsonplaceholder.typicode.com/posts/2";
+
+  makeAPICall(apiUrl1, handleData)
+    .then(handleData)
+    .catch((err) => console.log("error"));
+  makeAPICall(apiUrl2, handleData)
+    .then(handleData)
+    .catch((err) => console.log("error"));
+}
+
+main();
